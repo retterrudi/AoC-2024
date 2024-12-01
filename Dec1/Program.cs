a73a887b4957bae493b19316e0e7a8e06e2dd4f6
retterrudi <paul.serdack@gmail.com>
2024-12-01 13:57:55 +0100

    Solution Dec1-Part1

---
+++ Dec1/Program.cs
@@ -0,0 +1,10 @@
+﻿using Dec1.SolutionPart1;
+
+class Programm
+{
+    public static void Main(string[] args)
+    {
+        var solution = new SolutionPart1(args[0]);
+        solution.CalculateSolution();
+    }
+}
---
+++ Dec1/src/SolutionPart1/SolutionPart1.cs
@@ -0,0 +1,30 @@
+namespace Dec1.SolutionPart1;
+
+public class SolutionPart1(string input)
+{
+    public void CalculateSolution()
+    {
+        var lines = File.ReadLines(input);
+
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+
+        foreach (var line in lines)
+        {
+            var splitLine = line.Split("   ").Select(it => int.Parse(it)).ToList();
+            left.Add(splitLine[0]);
+            right.Add(splitLine[1]);
+        }
+        
+        left.Sort();
+        right.Sort();
+
+        var sum = 0;
+        for (int i = 0; i < left.Count; i++)
+        {
+            sum += Math.Abs(left[i] - right[i]);
+        }
+        
+        Console.WriteLine(sum);
+    }
+}
