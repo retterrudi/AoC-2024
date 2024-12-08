dce7c4014fe8f6737b49be4143a5bdb668dc2929
retterrudi <paul.serdack@gmail.com>
2024-12-08 13:31:42 +0100

    Add Part 2 of Dec04

--- Dec04/Program.cs
+++ Dec04/Program.cs
@@ -6,5 +6,7 @@ internal class Program
     {
         var solution1 = new SolutionPart1(args[0]);
         solution1.CalculateSolution();
+        var solution2 = new SolutionPart2(args[0]);
+        solution2.CalculateSolution();
     }
 }
---
+++ Dec04/src/SolutionPart2.cs
@@ -0,0 +1,42 @@
+namespace Dec04;
+
+public class SolutionPart2(string inputFile)
+{
+    public void CalculateSolution()
+    {
+        var lines = File.ReadAllLines(inputFile);
+        var array = lines.Select(line => line.ToCharArray()).ToArray();
+
+
+        var matchCounter = FindWords(array);
+        Console.WriteLine($"Solution Part 1: {matchCounter}");
+    }
+
+    private int FindWords(char[][] array)
+    {
+        var matchCounter = 0;
+        for (var y = 1; y < array.Length - 1; y++)
+        {
+            for (var x = 1; x < array[0].Length - 1; x++)
+            {
+                if (array[y][x] == 'A' && IsMatch(x, y, array))
+                {
+                    matchCounter++;
+                }
+            }
+        }
+
+        return matchCounter;
+    }
+
+    private bool IsMatch(int x, int y, char[][] array)
+    {
+        bool upperLeftLowerRight = (array[y - 1][x - 1] == 'M' && array[y + 1][x + 1] == 'S') 
+                                   || (array[y - 1][x - 1] == 'S' && array[y + 1][x + 1] == 'M');
+
+        bool upperRightLowerLeft = (array[y - 1][x + 1] == 'M' && array[y + 1][x - 1] == 'S') 
+                                   || (array[y - 1][x + 1] == 'S' && array[y + 1][x - 1] == 'M');
+            
+        return upperRightLowerLeft && upperLeftLowerRight;
+    }
+}
